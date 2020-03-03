a184114c982dae8e1ec466a7c22e8cbff3fe4f95
talves <tony@3alves.com>
2020-03-03 14:37:07 -0800

    added all lifecycle methods

--- plugins/debug/index.js
+++ plugins/debug/index.js
@@ -7,52 +7,106 @@ function pluginDebug(config) {
     name: "debug-plugin",
     // Hook into lifecycles
     onInit: options => {
-      console.log("[onInit:init] Build Starting 🔥");
+      console.log("Build Starting 🔥 - Before everythings");
       console.log(`${config.options && config.options.message}`);
+      // formatPrint(options);
+    },
+    onPreGetCache: options => {
+      console.log("Prior to on get cache 🐾");
       // formatPrint(options);
     },
     onGetCache: options => {
       console.log("[onGetCache:getCache] Fetching the last build’s cache 🐾");
       // formatPrint(options);
     },
+    onPostGetCache: options => {
+      console.log("After on get cache 🐾");
+      // formatPrint(options);
+    },
+    onPreInstall: options => {
+      console.log("Prior to on install ⚙");
+      // formatPrint(options);
+    },
     onInstall: options => {
-      console.log("[onInstall:install] Project dependencies are installing ⚙");
+      console.log("Project dependencies are installing ⚙");
+      // formatPrint(options);
+    },
+    onPostInstall: options => {
+      console.log("After on install ⚙");
       // formatPrint(options);
     },
     onPreBuild: options => {
-      console.log(
-        "[onPreBuild:preBuild] Prior to building the functions and running the build commands 🎢"
-      );
+      console.log("Prior to on build ✨");
       // formatPrint(options);
     },
     onBuild: options => {
       console.log("[onBuild:build] Build commands are executing ✨");
       // formatPrint(options);
     },
+    onPostBuild: options => {
+      console.log("After to on build ✨");
+      // formatPrint(options);
+    },
+    onPreFunctionsBuild: options => {
+      console.log("Prior to on functions build 🧨");
+      // formatPrint(options);
+    },
     onFunctionsBuild: options => {
       console.log(
-        "[onFunctionsBuild:functionsBuild] Serverless functions are building, if they exist on the site 🧨"
+        "Serverless functions are building, if they exist on the site 🧨"
       );
+      // formatPrint(options);
+    },
+    onPostFunctionsBuild: options => {
+      console.log("After on functions build 🧨");
+      // formatPrint(options);
+    },
+    onPreFunctionsPackage: options => {
+      console.log("Prior to on functions package 🎁");
       // formatPrint(options);
     },
     onFunctionsPackage: options => {
-      console.log(
-        "[onFunctionsPackage:package] Packaging functions to be deployed 🎁"
-      );
+      console.log("Packaging functions to be deployed 🎁");
+      // formatPrint(options);
+    },
+    onPostFunctionsPackage: options => {
+      console.log("Prior to on functions package 🎁");
       // formatPrint(options);
     },
     onPreDeploy: options => {
-      console.log(
-        "[onPreDeploy:preDeploy] Prior to the built package being deployed 🔮"
-      );
+      console.log("Prior to the built artifacts being deployed 🔮");
+      // formatPrint(options);
+    },
+    onDeploy: options => {
+      console.log("Built artifacts are deployed 🔮");
+      // formatPrint(options);
+    },
+    onPostDeploy: options => {
+      console.log("After built artifacts being deployed 🔮");
+      // formatPrint(options);
+    },
+    onPreSaveCache: options => {
+      console.log("Prior to saving cached assets 🎨");
       // formatPrint(options);
     },
     onSaveCache: options => {
-      console.log("[onSaveCache:saveCache] Saving cached assets 🎨");
+      console.log("Saving cached assets 🎨");
       // formatPrint(options);
     },
+    onPostSaveCache: options => {
+      console.log("After saving cached assets 🎨");
+      // formatPrint(options);
+    },
+    onSuccess: options => {
+      console.log("Sucessful Build! ✨🚀✨");
+      // formatPrint(options);
+    }
+    onError: options => {
+      console.log("Unsucessful Build 👎☠💀");
+      // formatPrint(options);
+    }
     onEnd: options => {
-      console.log("[onEnd:finally] Site build finished, site deployed! 🚀");
+      console.log("Site build finished, site deployed! 🚀");
       // formatPrint(options);
     }
   };
