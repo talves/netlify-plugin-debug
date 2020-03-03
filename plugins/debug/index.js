6c9d7d1ee2b2d3aa4b85f81c309c1f73a04897e4
talves <tony@3alves.com>
2020-03-03 12:00:57 -0800

    add print of lifecycle function options

--- plugins/debug/index.js
+++ plugins/debug/index.js
@@ -1,45 +1,59 @@
+function formatPrint(text) {
+  console.log(JSON.stringify(text, null, 2));
+}
+
 function pluginDebug(config) {
   return {
     name: "debug-plugin",
     // Hook into lifecycles
-    onInit: () => {
+    onInit: options => {
       console.log("[onInit:init] Build Starting 🔥");
       console.log(`${config.options && config.options.message}`);
+      formatPrint(options);
     },
-    onGetCache: () => {
+    onGetCache: options => {
       console.log("[onGetCache:getCache] Fetching the last build’s cache 🐾");
+      formatPrint(options);
     },
-    onInstall: () => {
+    onInstall: options => {
       console.log("[onInstall:install] Project dependencies are installing ⚙");
+      formatPrint(options);
     },
-    onPreBuild: () => {
+    onPreBuild: options => {
       console.log(
         "[onPreBuild:preBuild] Prior to building the functions and running the build commands 🎢"
       );
+      formatPrint(options);
     },
-    onBuild: () => {
+    onBuild: options => {
       console.log("[onBuild:build] Build commands are executing ✨");
+      formatPrint(options);
     },
-    onFunctionsBuild: () => {
+    onFunctionsBuild: options => {
       console.log(
         "[onFunctionsBuild:functionsBuild] Serverless functions are building, if they exist on the site 🧨"
       );
+      formatPrint(options);
     },
-    onFunctionsPackage: () => {
+    onFunctionsPackage: options => {
       console.log(
         "[onFunctionsPackage:package] Packaging functions to be deployed 🎁"
       );
+      formatPrint(options);
     },
-    onPreDeploy: () => {
+    onPreDeploy: options => {
       console.log(
         "[onPreDeploy:preDeploy] Prior to the built package being deployed 🔮"
       );
+      formatPrint(options);
     },
-    onSaveCache: () => {
+    onSaveCache: options => {
       console.log("[onSaveCache:saveCache] Saving cached assets 🎨");
+      formatPrint(options);
     },
-    onEnd: () => {
+    onEnd: options => {
       console.log("[onEnd:finally] Site build finished, site deployed! 🚀");
+      formatPrint(options);
     }
   };
 }
